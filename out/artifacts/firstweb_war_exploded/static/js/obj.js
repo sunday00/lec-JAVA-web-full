85f5bad900e2a031c4490647afc18176ed34a505
sunday00 <kind65505256@gmail.com>
2020-05-20 00:59:49 +0900

    report

---
+++ out/artifacts/firstweb_war_exploded/static/js/obj.js
@@ -0,0 +1,38 @@
+const data = {
+    "debug": "on",
+    "window": {
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": {
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+}
+
+let arr = [];
+function findNumber ( o ) {
+     for( k in o ){
+         if ( typeof o[k] == "object" ) findNumber(o[k]);
+         else if ( typeof o[k] == "number" ) arr.push(k);
+     }
+}
+
+findNumber(data);
+
+console.log( arr );
