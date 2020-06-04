e3efc73bb7d6534d13151568a57462a566e2173f
sunday00 <kind65505256@gmail.com>
2020-06-04 23:46:39 +0900

    front end ...

--- 02_call/app.js
+++ 02_call/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let apple = {
     color: "red",
     getColor() {
@@ -13,3 +15,42 @@ let banana = {
 }
 
 banana.getColor.call(apple);
+
+function cherry (a, b) {
+    this.a = a;
+    this.b = b;
+    this.sum = function () { return this.a + this.b; }
+    this.lazySum = function () {
+        setTimeout(function () {
+            console.log( this.a + this.b );
+        }, 500);
+    }
+    this.lazySum2 = function () {
+        setTimeout(function () {
+            console.log( this.a + this.b );
+        }.bind(this), 500);
+    }
+    this.lazySum3 = () => {
+        setTimeout(() => {
+            console.log( this.a + this.b );
+        }, 500);
+    }
+    this.lazySum4 = () => {
+        setTimeout(function () {
+            console.log( this.a + this.b );
+        }.bind(this), 500);
+    }
+}
+
+let c = new cherry(1, 3);
+
+c.lazySum();
+c.lazySum2();
+c.lazySum3();
+c.lazySum4();
+
+
+function abc () {}
+
+console.log( typeof abc );
+console.log( typeof abc.bind(this) );
